935893bb015419bd54eec6cd404eed911cd8c56c
Abdelrahman Elsisi <abdelrahman.m.elsisi@gmail.com>
2023-12-22 01:22:53 +0200

    fix: scroll icon. fix: menu button functionality

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-// variables nad Consts
+// variables and Consts
 const menu = document.getElementById("menu-icon");
 const list = document.getElementById("menu-list");
 
 //add Events
 menu.addEventListener("click",function(){
-    list.style.display = "block";
-});
-
-document.addEventListener("click",function(event){
-    const targetEle = event.target;
-    if(targetEle.classList.contains("menu-icon"))
-        console.log("YESS");
-    else
-        list.style.display = "none";
+    list.style.display = (list.style.display == "none") ? "block" : "none";
 });
 
 // scroll to top function
-function scrolltoTop() {
-    let currOffsetY = scrollY;
-    const scrollStep = 100000;
+function scrollPage() {
+    const scrollStep = 10;
     const scrollInt = setInterval(function(){
         //scroll to top
-        const currScrollY = scrollY;
-        if(currScrollY == 0) 
+        if(scrollY == 0) 
             clearInterval(scrollInt);
         else
-            scrollTo(0,currScrollY-scrollStep);
-        console.log(currScrollY);
-
-    },50);
+            scrollTo(0,scrollY-scrollStep);
+    },1);
 }
