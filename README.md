70a83a14e4f92c6dfd62ad70b8ca987042fbd7d0
Abdelrahman Elsisi <abdelrahman.m.elsisi@gmail.com>
2023-12-22 17:03:28 +0200

    feature: progress-bar added

--- README.md
+++ README.md
@@ -3,3 +3,26 @@ I am happy to share my first front end responsive project using HTML CSS and JS
 I am looking forward to improve my skills and work on more intersting and chanllengin projects.
 I followed Osama Elzero Tutorial for this Temp.
 Added some JS to make it alive.
+
+for HTML:
+-viewport used for responsiveness
+-inline style for some elements. so, property value is accessible using JS
+-added comments for clearness
+
+for CSS:
+-CSS Grid
+-CSS Flex-box
+-Media Query
+-positioning elements
+-CSS variables
+-different CSS Selectors
+
+JS Features added:
+-scroll bar
+-scroll to top button
+-menu button to toggle menu UL style display
+
+Libraries:
+-normalize to reset CSS elements for different browsers
+-font-awesome for icons
+-google-fonts
--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 const menu = document.getElementById("menu-icon");
 const list = document.getElementById("menu-list");
 
-//add Events
+//progress bar scrolling event
+window.addEventListener("scroll", () => {
+    const progressbar = document.querySelector(".progress-bar");
+    const scrollHeight = document.body.scrollHeight - window.innerHeight; // Total scrollable height
+    const scrollPosition = window.scrollY; // Current scroll position
+    const progress = scrollPosition / scrollHeight; // Percentage of scrolled space
+  
+    progressbar.style.width = `${progress * 100}%`; // Set width as percentage
+});
+  
+//menu button click event
 menu.addEventListener("click",function(){
     list.style.display = (list.style.display == "none") ? "block" : "none";
 });
